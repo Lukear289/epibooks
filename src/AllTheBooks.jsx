e7e126146b99699e5a4176b0efe0d9a602b44cb8
Luca Armagno <luca.armagno289@gmail.com>
2024-01-30 19:32:40 +0100

    commit

--- src/AllTheBooks.jsx
+++ src/AllTheBooks.jsx
@@ -2,27 +2,32 @@ import { Component } from 'react';
 import Card from 'react-bootstrap/Card';
 import Button from 'react-bootstrap/Button';
 import MyBooks from '../src/data/scifi.json';
-import { Container } from 'react-bootstrap';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 class AllTheBooks extends Component {
   render() {
     return (
       <>
         <Container>
-          <Row></Row>
+          <Row>
+            {MyBooks.map((scifi) => {
+              return (
+                <Col xs={12} md={4}>
+                  <Card style={{ width: '15rem' }}>
+                    <Card.Img variant="top" src={scifi.img} />
+                    <Card.Body>
+                      <Card.Title>{scifi.title}</Card.Title>
+                      <Card.Text>{scifi.category}</Card.Text>
+                      <Button variant="primary"> {scifi.price}</Button>
+                    </Card.Body>
+                  </Card>
+                </Col>
+              );
+            })}
+          </Row>
         </Container>
-        {MyBooks.map((scifi) => {
-          return;
-
-          <Card style={{ width: '18rem' }}>
-            <Card.Img variant="top" src={scifi.img} />
-            <Card.Body>
-              <Card.Title>{scifi.title}</Card.Title>
-              <Card.Text></Card.Text>
-              <Button variant="primary"></Button>
-            </Card.Body>
-          </Card>;
-        })}
       </>
     );
   }
--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import MyNav from './MyNav';
 import MyFooter from './MyFooter';
 import AllTheBooks from './AllTheBooks';
+
 function App() {
   return (
     <div>
